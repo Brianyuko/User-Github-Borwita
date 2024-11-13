ee1df4c486109e83e7640fb0b140522fcca52902
Brianyuko <46413112+Brianyuko@users.noreply.github.com>
2024-11-13 11:10:57 +0700

    feat: tab bar view (#2)

---
+++ lib/presentation/pages/favorite_page_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FavoritePageView extends StatelessWidget {
+  const FavoritePageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Favorite'),
+    );
+  }
+}
---
+++ lib/presentation/pages/popular_page_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PopularPageView extends StatelessWidget {
+  const PopularPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Popular'),
+    );
+  }
+}
